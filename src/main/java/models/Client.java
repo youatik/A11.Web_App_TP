db2176dc435bd00e9ecf85d4f99d5ad9b45c0ccf
youatik <112843400+youatik@users.noreply.github.com>
2023-06-06 19:34:40 -0400

    Imported POJO models package from previous iteration

---
+++ src/main/java/models/Client.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author youatik
+ */
+public class Client {
+    private int clientId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+
+    public Client() {
+        // Default constructor
+    }
+
+    public Client(int clientId, String firstName, String lastName, String email, String address) {
+        this.clientId = clientId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+    }
+
+    // Getters and Setters
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    @Override
+    public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Client{")
+      .append("clientId=").append(clientId)
+      .append(", firstName='").append(firstName).append('\'')
+      .append(", lastName='").append(lastName).append('\'')
+      .append(", email='").append(email).append('\'')
+      .append(", address='").append(address).append('\'')
+      .append('}');
+    return sb.toString();
+}
+}
+
