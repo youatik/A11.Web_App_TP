7c0934dd1dfaead3aef3101ef5b7d02e88023bce
youatik <112843400+youatik@users.noreply.github.com>
2023-06-06 20:57:18 -0400

    Added Library DAO Interface and Implementation

---
+++ src/main/java/data/LibraryDAOInterface.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package data;
+import models.Library;
+/**
+ *
+ * @author youatik
+ */
+import java.util.List;
+
+public interface LibraryDAOInterface {
+    void addLibrary(Library library);
+    
+    void updateLibrary(Library library);
+    
+    void deleteLibrary(long eanIsbn13);
+    
+    Library getLibraryById(long eanIsbn13);
+    
+    List<Library> findByTitle(String word);
+
+    List<Library> findByCreators(String word);
+
+    List<Library> findByDescription(String word);
+    
+    List<Library> getAllLibraries();
+}
